bff1771b3fa5e73700515b865b4cb7168f95d1c3
Andre <demchyshyn95@gmail.com>
2020-12-07 00:09:59 +0200

    update usersRouters

--- lesson4/routers/usersRouters.js
+++ lesson4/routers/usersRouters.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
-const { usersControllers: {getUsers,updateUserById,deleteUser,createNewUser,getUserById_Car } } = require('../controllers');
-const { middlewaresUsers } = require('../middlewares');
+const { usersControllers: { getUsers,updateUserById,deleteUser,createNewUser,getUserById_Car } } = require('../controllers');
+const { middlewaresUsers: { getUserById,createNewUser,deleteUser } } = require('../middlewares');
 
 const usersRouters = Router();
 
 usersRouters.get('/', getUsers);
-usersRouters.get('/:id', middlewaresUsers.getUserById, getUserById_Car);
-usersRouters.post('/', middlewaresUsers.createNewUser, createNewUser);
-usersRouters.put('/:id', middlewaresUsers.deleteUser, updateUserById);
-usersRouters.delete('/:id', middlewaresUsers.deleteUser, deleteUser);
+usersRouters.get('/:id', getUserById, getUserById_Car);
+usersRouters.post('/', createNewUser, createNewUser);
+usersRouters.put('/:id', deleteUser, updateUserById);
+usersRouters.delete('/:id', deleteUser, deleteUser);
 
 module.exports = usersRouters;
